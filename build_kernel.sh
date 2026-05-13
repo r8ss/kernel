a81fdead86e3e3c8f342feff868c9a08ac103c5d
redmax19 <alessandroaraujo0098@gmail.com>
2026-05-13 09:09:09 -0300

    Add files via upload

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-export PLATFORM_VERSION=11
-export ANDROID_MAJOR_VERSION=r 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 export ARCH=arm64
-export SEC_BUILD_CONF_VENDOR_BUILD_OS=13
+export PATH="$SCRIPT_DIR/toolchain/clang-r416183b/bin:${PATH}"
+export LLVM=1
+export LLVM_IAS=1
 export CROSS_COMPILE=aarch64-linux-gnu-
-make exynos9830_defconfig
-make -j$(nproc)
+
+scripts/kconfig/merge_config.sh -m -O . arch/arm64/configs/exynos9830_defconfig arch/arm64/configs/r8s.config arch/arm64/configs/ksu.config
+make KCONFIG_ALLCONFIG=.config alldefconfig
+make --jobs=$(nproc --all)
